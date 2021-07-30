6fd4d0eab9067c905c12c662102996900878da01
diegowanborges <diegowanborges@gmail.com>
2021-07-30 10:56:01 -0300

    Added config server

---
+++ tk-config-server/src/main/java/com/twokeys/configserver/TkConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.twokeys.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class TkConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkConfigServerApplication.class, args);
+	}
+
+}
---
+++ tk-config-server/src/test/java/com/twokeys/configserver/TkConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twokeys.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
